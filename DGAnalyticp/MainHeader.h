208e87aa6089152b88537c7ebb6a331a7f387e32
mhmtakif <akif@dijitalgaraj.com>
2017-09-13 15:57:52 +0300

    DGAnalytic.h -> MainHeader.h

--- DGAnalyticp/MainHeader.h
+++ DGAnalyticp/MainHeader.h
@@ -3,16 +3,16 @@
 //  VersionBox
 //
 //  Created by Mehmet Akif Şengül on 23.03.2017.
-//  Copyright © 2017 Mehmet Akif Şengül. All rights reserved.
+//  Copyright © 2017 Dijital Garaj. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 //! Project version number for DGAnalyticp.
-FOUNDATION_EXPORT double DGAnalyticVersionNumber;
+FOUNDATION_EXPORT double VersionBoxVersionNumber;
 
 //! Project version string for DGAnalyticp.
-FOUNDATION_EXPORT const unsigned char DGAnalyticVersionString[];
+FOUNDATION_EXPORT const unsigned char VersionBoxVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DGAnalyticp/PublicHeader.h>
 
